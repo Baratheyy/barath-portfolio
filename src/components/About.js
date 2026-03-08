a7ce15ddf3dab996eb625eb1e41aa96e364a0874
Barath <bharathplkm1234@gmail.com>
2026-03-08 18:35:12 +0530

    fix build errors

--- src/components/About.js
+++ src/components/About.js
@@ -2,21 +2,43 @@ export default function About(){
 
 return(
 
-<section id="about">
+<section id="about" style={{
+padding:"120px 80px",
+background:"#020617",
+color:"white"
+}}>
 
-<h2 style={{fontSize:"40px",color:"#38bdf8"}}>
+<h2 style={{
+fontSize:"42px",
+color:"#38bdf8",
+marginBottom:"20px"
+}}>
 
 About Me
 
 </h2>
 
-<p style={{maxWidth:"700px",marginTop:"20px"}}>
+<p style={{
+maxWidth:"750px",
+lineHeight:"1.8",
+fontSize:"18px"
+}}>
 
-Hello! I'm Barath V, pursuing MSc Integrated Computer Science at
-College of Engineering Guindy, Anna University.
+Hello! I'm <b>Barath V</b>, currently pursuing 
+<b> M.Sc Integrated Computer Science </b> at 
+<b> College of Engineering Guindy (Anna University)</b>.
 
-I enjoy building web applications and solving algorithmic
-problems using data structures.
+<br/><br/>
+
+I am passionate about <b>software development</b>, 
+<b>full-stack web applications</b>, and solving complex 
+<b>algorithmic problems</b> using data structures.
+
+<br/><br/>
+
+I enjoy building scalable applications, exploring new 
+technologies, and improving my problem-solving skills 
+through competitive programming.
 
 </p>
 
